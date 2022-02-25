05af6473fe4260eb06c79d342176444afd7a1fbf
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-02-25 15:02:36 +0000

    Initial Commit

---
+++ MusicByLocationSwift/MusicByLocationSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationSwiftApp.swift
+//  MusicByLocationSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
